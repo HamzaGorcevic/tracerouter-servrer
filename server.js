47ec95e539b07083082d446ac037688e5fa21d5d
HamzaGorcevic <gorcevichamza76@gmail.com>
2024-02-24 23:34:05 +0100

    fix:added start script

--- server.js
+++ server.js
@@ -10,20 +10,6 @@ const port = process.env.PORT || 8080;
 const ipAddress = "0.0.0.0";
 
 app.use(express.static(__dirname));
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With,content-type"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-});
-
 app.use(cors()); // Enable CORS for all routes
 
 const Traceroute = require("nodejs-traceroute");
@@ -78,10 +64,31 @@ app.get("/traceroute", async (req, res) => {
     console.log(host);
 
     try {
-        let hops = await getListOfHops(host);
+        const hops = await new Promise((resolve, reject) => {
+            const tracer = new Traceroute();
+            const hops = [];
+
+            tracer.on("hop", (hop) => {
+                console.log(hop);
+                hops.push(hop);
+            });
+
+            tracer.on("close", () => {
+                resolve(hops);
+            });
+
+            tracer.on("error", (err) => {
+                reject(err);
+            });
+
+            tracer.on("end", () => {
+                resolve(hops);
+            });
+
+            tracer.trace(host);
+        });
 
         hops.unshift({ ip: "" });
-        console.log(hops);
 
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.json(hops);
