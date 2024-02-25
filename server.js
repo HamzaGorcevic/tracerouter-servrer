77888d6f36f59d96116ad59446582be0e5715460
HamzaGorcevic <gorcevichamza76@gmail.com>
2024-02-25 15:18:45 +0100

    a10

--- server.js
+++ server.js
@@ -8,27 +8,29 @@ const app = express();
 const port = process.env.PORT || 8080;
 const ipAddress = "0.0.0.0";
 
-// Set middleware of CORS
-app.use(cors()); // Enable CORS for all routes
 app.use(express.static(__dirname));
 app.use(express.json());
 
-// Set CORS headers
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Allow requests from any origin
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Private-Network", true);
-    res.setHeader("Access-Control-Max-Age", 7200); // Set max age for preflight requests
-    next();
-});
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://hamzagorcevic.github.io/traceroute",
+];
+
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        methods: "GET,POST,PUT,DELETE,HEAD,PATCH",
+        allowedHeaders: "",
+        credentials: false,
+        maxAge: 8000,
+    })
+);
 
 // Define your routes
 app.post("/traceroute", async (req, res) => {
